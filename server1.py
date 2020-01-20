2847e6b2236e60453668c13b4683c606e49eb660
mfern13 <59903046+mfern13@users.noreply.github.com>
2020-01-19 19:31:11 -0500

    Add files via upload

---
+++ server1.py
@@ -0,0 +1,13 @@
+# Code taken from official Python documentation:
+# https://docs.python.org/3/library/http.server.html
+
+import http.server
+import socketserver
+
+PORT = 8000
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
---
+++ start server.bat
@@ -0,0 +1,1 @@
+python server1.py
